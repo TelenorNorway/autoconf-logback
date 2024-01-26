095ce4816b4d5021d90d0309ca132662a2d98628
Jameson <james.bradlee@telenor.no>
2024-01-26 18:48:25 +0100

    fix logback not configuring in standalone

--- autoconf-logback-configuration-standalone/build.gradle.kts
+++ autoconf-logback-configuration-standalone/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
 	implementation(project(":autoconf-logback-configure"))
 	implementation("ch.qos.logback:logback-classic:1.4.14")
+	implementation("ch.qos.logback:logback-core:1.4.14")
 	api("org.slf4j:slf4j-api:2.0.9")
 }
--- autoconf-logback-configuration-standalone/src/main/kotlin/no/telenor/kt/internal/autoconf/logback/configuration/standalone/Initializer.kt
+++ autoconf-logback-configuration-standalone/src/main/kotlin/no/telenor/kt/internal/autoconf/logback/configuration/standalone/Initializer.kt
@@ -4,12 +4,10 @@ import ch.qos.logback.classic.LoggerContext
 import ch.qos.logback.classic.spi.Configurator
 import ch.qos.logback.core.spi.ContextAwareBase
 import no.telenor.kt.autoconf.logback.configure.Configure
-import org.slf4j.LoggerFactory
 
 internal class Initializer : ContextAwareBase(), Configurator {
 	override fun configure(ctx: LoggerContext): Configurator.ExecutionStatus {
-		val context = LoggerFactory.getILoggerFactory() as LoggerContext
-		Configure.configure(context)
+		Configure.configure(ctx)
 		return Configurator.ExecutionStatus.DO_NOT_INVOKE_NEXT_IF_ANY
 	}
 }
